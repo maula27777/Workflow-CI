4a96d3574f1ecaa6d3fa75f21b4a9202a9a51e1d
maula27777 <ahmadmaula2703@gmail.com>
2025-12-22 22:07:10 +0700

    simplify modelling.py for CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,38 +1,21 @@
 import pandas as pd
-import numpy as np
-import mlflow
-import mlflow.sklearn
-
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-
-# ===== 1. Load data =====
-data = pd.read_csv("heart_processed.csv")
+data = pd.read_csv("MLProject/heart_processed.csv")
 
 X = data.drop("target", axis=1)
 y = data["target"]
 
-
-# ===== 2. Split data =====
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-# ===== 3. Training + MLflow Tracking =====
-mlflow.set_experiment("model-klasifikasi")
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-with mlflow.start_run():
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-
-    mlflow.log_param("model_type", "LogisticRegression")
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
-
-    print("Accuracy:", acc)
+print("Accuracy:", acc)
